68b94a9bf5622d3041645555834af4df93af1a96
weyller <weyller@gmail.com>
2017-07-30 01:38:30 -0400

    4th commit ver2 - wrapper json

--- js/main.js
+++ js/main.js
@@ -1,41 +1,227 @@
-	$(function(){
+////=========  Mis en valeur globale  ===================
+var jsonData; 
+
+var itemsInJson = [];
+var phone =[];
+var email = [];
+var brand = [];
+var title = [];
+var description = [];
+var local = [];
+var address = [];
+var postal = [];
+var price = [];
+
+
+////-------------------
+var arrayPictures = [];
+var pictures = [];
+
+$(function(){
 	
 	'use strict';
-        
-        alert("test");
-        
-   $('#dix').mouseover(function(){
-    $('#dix').hide(); 
-       
-       
-       
-       
-       
-       
-       
-});    
-   //----------------------
-  
-          $('#free').mouseover(function(){
-    $('#free').hide(); 
-       
-});    
-        
-   //-----------------------     
-  $(".gallery img").mouseout(function(){
-    $('#dix').show(); 
-      $('#free').show(); 
-});   
+    
+    alert("test");
      
- //-----------------------
-$('#sammy').click(function openSammy() {
-    window.open("http://www.sammydress.com/");
-})   ;     
-
-$('#wrapper1 img').mouseover(fun){
-  
-      alert("over");
+ 		$.getJSON("json/dataListing.json", function(resultat) {
+			 jsonData = resultat;
+			
+			// alert("la valeur resultat: " + resultat);
+			 
+	
+		parseJSON();
+        
+        displayData();
+			
+		
+           console.log("items: " + itemsInJson);
+ 		 	console.log(phone);	
+         	console.log(email);
+            console.log(brand);
+            console.log(title);
+            console.log(description);
+            console.log(local);	
+         	console.log(address);
+            console.log(postal);
+            console.log(price);
+            console.log(pictures);
+			
+			
+		});
+        
  
 });
         
+
+// fonction qui va parcourir et parser le fichier Json contenant lers details des menus
+		function parseJSON(){
+		'use strict';
+			
+			
+            console.log( jsonData.PROFIL["DETAILS"]);
+			for(var item in jsonData.PROFIL){
+				
+			 
+				if(jsonData.PROFIL.hasOwnProperty(item)){
+					itemsInJson.push(item);
+ 					phone.push(jsonData.PROFIL[item][0]);
+ 					email.push(jsonData.PROFIL[item][1]);
+  				  	brand.push(jsonData.PROFIL[item][2]);
+ 					title.push(jsonData.PROFIL[item][3]);
+ 					description.push(jsonData.PROFIL[item][4]);
+ 					local.push(jsonData.PROFIL[item][5]);
+ 
+                    address.push(jsonData.PROFIL[item][6]);
+ 					postal.push(jsonData.PROFIL[item][7]);
+ 					price.push(jsonData.PROFIL[item][8]);
+ 					pictures.push(jsonData.PROFIL[item][9]);
+ 			 
+                }
+           
+            }
+
+            
+        }
+ 
+
+function displayData(){
+	
+	'use strict';
+	
+	//============================== parametre toujours en MAJUSCULE ===
+	var body = document.getElementsByTagName("SECTION")[1];
     
+    body.setAttribute("class", "mainSection");
+	
+	
+	for(var i = 0; i< itemsInJson.length; i++){
+		
+		//-------- On cre des objet pour le data ------
+		
+		//============================== parametre toujours en MAJUSCULE 
+        var container = document.createElement('DIV');
+        container.setAttribute("class","container section1")
+        //----------
+        var row = document.createElement('DIV');
+        row.setAttribute("class","row");
+        row.setAttribute("id","main");
+        //---------
+        var col = document.createElement('DIV');
+        col.setAttribute("class","col-sm-6")
+        
+		var wrapperRow = document.createElement("DIV"); 
+		
+		wrapperRow.setAttribute("class", "row");
+		//---------------------------
+        var mainTitle =      document.createElement('H2');
+        mainTitle.setAttribute("id","title");
+        
+        //$('H2').html("MY TITLE");
+        mainTitle.innerHTML = title[i];
+      
+        //---------------------------
+		var textTitle = document.createTextNode(title[i]);
+		var textStaring = document.createTextNode(description[i]);
+		var textPrice = document.createTextNode(price[i]);
+        
+     
+		
+		//============================== parametre toujours en MAJUSCULE ===
+		var images = document.createElement("IMG");
+		images.setAttribute("src", "images/" + pictures[0][1]);
+        images.setAttribute("width", "350" );
+        images.setAttribute("heigth", "230" );
+        
+        
+        //--------------------------------------------------
+	 	//$("IMG").css({"width": "300", "height": "200"});
+		
+		//-------------- On des div pour afficher ---------------------
+		var divForImages = document.createElement("DIV");
+		divForImages.setAttribute("id", "divForImages");
+		
+		var divForTilte = document.createElement("DIV");
+		divForTilte.setAttribute("id", "divForTilte");
+		
+		var divForStars = document.createElement("DIV");
+		divForStars.setAttribute("id", "divForStars");
+		
+		var divForPrices = document.createElement("DIV");
+		divForPrices.setAttribute("id", "divForPrices");
+		
+        var divForboutton = document.createElement("DIV");
+		divForboutton.setAttribute("id", "divForboutton");
+     
+		
+  
+        
+		//---------- BOUTON COMMANDER--------------
+//             var bouton = document.createElement('INPUT');
+//            bouton.setAttribute('type', 'button');
+//            bouton.setAttribute('id', 'idBouton');
+//            bouton.setAttribute('value', 'COMMANDER');
+//            bouton.setAttribute('onclick', 'funcCalcul()');
+
+
+         
+		// Textnode pour la quatite de plats commande
+		var textQte = document.createTextNode("Quantité: ")
+		
+        
+          
+		
+//            divForboutton.appendChild(bouton);
+//			divForboutton.appendChild(qte);
+//		    divForboutton.appendChild(textQte);
+		
+        //     divForboutton.appendChild(bouton2);
+        
+        
+        divForTilte.appendChild(textTitle);
+        
+		divForImages.appendChild(images);
+		
+		divForStars.appendChild(textStaring);
+		
+		divForPrices.appendChild(textPrice);
+        
+        //--------------------------------------
+        divForPrices.appendChild(divForboutton);
+        
+         var textH3 = document.createElement("H3");
+       // $("H3").val({"COMMANDER"});
+		
+      //  divForboutton.appendChild(textH3);
+        
+        
+        
+        
+        
+        
+		
+		//-----------------------------
+		wrapperRow.appendChild(mainTitle);
+        
+      
+        
+		wrapperRow.appendChild(divForTilte);
+        wrapperRow.appendChild(divForImages);
+		wrapperRow.appendChild(divForStars);
+		wrapperRow.appendChild(divForPrices);
+        
+        
+       
+        
+           col.appendChild(wrapperRow);
+            
+        row.appendChild(col);
+		
+	 container.appendChild(row);
+
+    body.appendChild(container);
+		
+	
+	}
+	
+}
+
--- js/main2.js
+++ js/main2.js
@@ -1,42 +1,216 @@
-	$(function(){
+//=========  Mis en valeur globale  ===================
+var jsonData; 
+var phone =[];
+var email = [];
+var brand = [];
+var title = [];
+var description = [];
+var local = [];
+var address = [];
+var postal = [];
+var price = [];
+
+
+
+//-------------------
+var arrayPictures = [];
+var pictures = [];
+
+
+ // Debut de la fontion principale
+//---------------------------
+$(document).ready(function(){
+
+   // alert("test2");
+    
+	'use strict';
+		$.getJSON("json/dataListing.json", function(resultat) {
+			 jsonData = resultat;
+			
+			 //alert("la valeur resultat: " + resultat);
+			 console.log("The result in json data: " + jsonData);
+	
+	//	parseJSON();
+        
+      //  displayData();
+			
+		
+//            console.log("prices: " + picDes);
+//		 	console.log(des_names);	
+//         	console.log(priceDes);
+//            console.log(descriptionDes);
+//            console.log(sup_CoffDes);
+//            console.log(sup_vinDes);
+			
+			
+		});
+});
+
+// fonction qui va parcourir et parser le fichier Json contenant lers details des menus
+		function parseJSON(){
+		'use strict';
+			
+			
+			   //alert( jsonData.MENU["MARCHE"]);
+			for(var item in jsonData.MENU){
+				
+			 
+				if(jsonData.MENU.hasOwnProperty(item)){
+					plat_names.push(item);
+ 					pictures.push(jsonData.MENU[item][0]);
+					menuPlat.push(jsonData.MENU[item][1]);
+ 				  	price.push(jsonData.MENU[item][2]);
+					description.push(jsonData.MENU[item][3]);
+					sup_from.push(jsonData.MENU[item][4]);
+					sup_vin.push(jsonData.MENU[item][5]);
+ 
+ 			 
+                }
+           
+            }
+            //==================================== TEST
+            for(var item in jsonData.DESSERT){
+				
+			 
+				if(jsonData.DESSERT.hasOwnProperty(item)){
+					des_names.push(item);
+ 					picDes.push(jsonData.DESSERT[item][0]);
+					menuPlatDes.push(jsonData.DESSERT[item][1]);
+ 				  	priceDes.push(jsonData.DESSERT[item][2]);
+					descriptionDes.push(jsonData.DESSERT[item][3]);
+					sup_CoffDes.push(jsonData.DESSERT[item][4]);
+					sup_vinDes.push(jsonData.DESSERT[item][5]);
+ 
+ 			 
+                }
+           
+            }
+            console.log(jsonData.DESSERT)
+            //===================================
+            
+        }
+ 
+
+function displayData(){
 	
 	'use strict';
-        
-//        alert("test");
-
- //hideMe();
-
-
-    //-------------------------------    
-  $('#sammy').click(function openSammy() {
-    window.open("http://www.sammydress.com/");
-}) ; 
-       
-       
-});    
-//===========================================================================
-     
- function hideMe() {
- 	
-		 $('#wrapper1 img').mouseover(function(){
-		   $('.gallery #dix').hide(); 
-
-		      
-		});    
-		      
-	    //-----------------------     
-		 
-		$('#wrapper1 img').mouseout(function(){
-		   $('.gallery #dix').show(); 
-		});
-     
-     		        
 	
+	//============================== parametre toujours en MAJUSCULE ===
+	var body = document.getElementsByTagName("SECTION")[0];
+	
+	
+	for(var i = 0; i< plat_names.length; i++){
+		
+		//-------- On cre des objet pour le data ------
+		
+		//============================== parametre toujours en MAJUSCULE ===
+		var wrapper = document.createElement("DIV"); 
+		
+		wrapper.setAttribute("class", "wrapper");
+		
+		var textTitle = document.createTextNode(menuPlat[i]);
+		var textStaring = document.createTextNode(description[i]);
+		var textPrice = document.createTextNode(price[i]);
+        
+     
+		
+		//============================== parametre toujours en MAJUSCULE ===
+		var images = document.createElement("IMG");
+		images.setAttribute("src", "images/" + pictures[i]);
+        images.setAttribute("width", "350" );
+        images.setAttribute("heigth", "230" );
+        
+        
+        //--------------------------------------------------
+	 	//$("IMG").css({"width": "300", "height": "200"});
+		
+		//-------------- On des div pour afficher ---------------------
+		var divForImages = document.createElement("DIV");
+		divForImages.setAttribute("id", "divForImages");
+		
+		var divForTilte = document.createElement("DIV");
+		divForTilte.setAttribute("id", "divForTilte");
+		
+		var divForStars = document.createElement("DIV");
+		divForStars.setAttribute("id", "divForStars");
+		
+		var divForPrices = document.createElement("DIV");
+		divForPrices.setAttribute("id", "divForPrices");
+		
+        var divForboutton = document.createElement("DIV");
+		divForboutton.setAttribute("id", "divForboutton");
+     
+		
+  
+        
+		//---------- BOUTON COMMANDER--------------
+             var bouton = document.createElement('INPUT');
+            bouton.setAttribute('type', 'button');
+            bouton.setAttribute('id', 'idBouton');
+            bouton.setAttribute('value', 'COMMANDER');
+            bouton.setAttribute('onclick', 'funcCalcul()');
 
- }
-    
- //-----------------------
-    
 
-   
- 
+		// Input pour la quatite de plats commande
+		 var qte = document.createElement('INPUT');
+            qte.setAttribute('type', 'number');
+            qte.setAttribute('id', 'idQuantite');
+			qte.setAttribute('min', '1');
+			qte.setAttribute('max', '30');
+			qte.setAttribute("value", "0");
+         
+		// Textnode pour la quatite de plats commande
+		var textQte = document.createTextNode("Quantité: ")
+		
+        
+          
+		
+            divForboutton.appendChild(bouton);
+			divForboutton.appendChild(qte);
+		    divForboutton.appendChild(textQte);
+		
+        //     divForboutton.appendChild(bouton2);
+        
+        
+		divForImages.appendChild(images);
+		
+		divForTilte.appendChild(textTitle);
+		
+		divForStars.appendChild(textStaring);
+		
+		divForPrices.appendChild(textPrice);
+        
+        //--------------------------------------
+        divForPrices.appendChild(divForboutton);
+        
+         var textH3 = document.createElement("H3");
+       // $("H3").val({"COMMANDER"});
+		
+      //  divForboutton.appendChild(textH3);
+        
+        
+        
+        
+        
+        
+		
+		//-----------------------------
+		wrapper.appendChild(divForImages);
+		wrapper.appendChild(divForTilte);
+		wrapper.appendChild(divForStars);
+		wrapper.appendChild(divForPrices);
+        
+        
+       
+       
+        
+    
+		
+	 body.appendChild(wrapper);
+		
+		
+	
+	}
+	
+}
+
