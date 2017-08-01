89b75c66f5c7cd022e60eff9387693ad00267de2
weyller <weyller@gmail.com>
2017-07-31 20:37:32 -0400

    fourth commit - parsing json to html

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $(function(){
 	
 	'use strict';
     
-    alert("test");
+    //alert("test");
      
  		$.getJSON("json/dataListing.json", function(resultat) {
 			 jsonData = resultat;
@@ -58,7 +58,7 @@ $(function(){
 		'use strict';
 			
 			
-            console.log( jsonData.PROFIL["DETAILS"]);
+           // console.log( jsonData.PROFIL["DETAILS"]);
 			for(var item in jsonData.PROFIL){
 				
 			 
@@ -113,115 +113,262 @@ function displayData(){
 		
 		wrapperRow.setAttribute("class", "row");
 		//---------------------------
-        var mainTitle =      document.createElement('H2');
+        var mainTitle =  document.createElement('H2');
         mainTitle.setAttribute("id","title");
         
         //$('H2').html("MY TITLE");
         mainTitle.innerHTML = title[i];
-      
-        //---------------------------
-		var textTitle = document.createTextNode(title[i]);
-		var textStaring = document.createTextNode(description[i]);
-		var textPrice = document.createTextNode(price[i]);
-        
-     
-		
-		//============================== parametre toujours en MAJUSCULE ===
-		var images = document.createElement("IMG");
-		images.setAttribute("src", "images/" + pictures[0][1]);
-        images.setAttribute("width", "350" );
-        images.setAttribute("heigth", "230" );
-        
-        
-        //--------------------------------------------------
-	 	//$("IMG").css({"width": "300", "height": "200"});
-		
-		//-------------- On des div pour afficher ---------------------
-		var divForImages = document.createElement("DIV");
-		divForImages.setAttribute("id", "divForImages");
-		
-		var divForTilte = document.createElement("DIV");
-		divForTilte.setAttribute("id", "divForTilte");
-		
-		var divForStars = document.createElement("DIV");
-		divForStars.setAttribute("id", "divForStars");
-		
-		var divForPrices = document.createElement("DIV");
-		divForPrices.setAttribute("id", "divForPrices");
-		
-        var divForboutton = document.createElement("DIV");
-		divForboutton.setAttribute("id", "divForboutton");
-     
-		
   
-        
-		//---------- BOUTON COMMANDER--------------
-//             var bouton = document.createElement('INPUT');
-//            bouton.setAttribute('type', 'button');
-//            bouton.setAttribute('id', 'idBouton');
-//            bouton.setAttribute('value', 'COMMANDER');
-//            bouton.setAttribute('onclick', 'funcCalcul()');
-
-
-         
-		// Textnode pour la quatite de plats commande
-		var textQte = document.createTextNode("Quantité: ")
+        //-------------------------------------------------
 		
+        var divSellerPro = document.createElement('DIV');
+        divSellerPro.setAttribute("class","sellerProfil");
+        divSellerPro.setAttribute("height","60");
+        divSellerPro.setAttribute("width","800");
+        divSellerPro.setAttribute("style","background-color:red;");
+        //---------------------------------------------
+        var spanMap = document.createElement('SPAN');
+        spanMap.setAttribute("class","map");
+        //--------------------------------------------
+        var iMap = document.createElement('I');
+        iMap.setAttribute("class","fa fa-map-marker");
+        iMap.setAttribute("aria-hidden","true");
+        //--------------------------------------------
+        var textMap = document.createTextNode("Map");
+        //---------------------------------------------
+        var spanSell = document.createElement('SPAN');
+        spanMap.setAttribute("class","seller");
+        //--------------------------------------------
+        var isell = document.createElement('I');
+        isell.setAttribute("class","fa fa-user");
+        isell.setAttribute("aria-hidden","true");
+        //--------------------------------------------
+        var textSell = document.createTextNode("Seller's profil");
+        //---------------------------------------------
+        var spanSave = document.createElement('SPAN');
+        spanMap.setAttribute("class","favorite");
+        //--------------------------------------------
+        var iFavo = document.createElement('I');
+        iFavo.setAttribute("class","fa fa-heart-o");
+        iFavo.setAttribute("aria-hidden","true");
+        //--------------------------------------------
+        var textSave = document.createTextNode("Save");
+        //----------------------------------------
+        var aImgFocus = document.createElement('A');
+        aImgFocus.setAttribute("href","#");
+        aImgFocus.setAttribute("class","focusImg");
+        //---------------------------------------
+        var images = document.createElement("IMG");
+		images.setAttribute("src", "images/" + pictures[0][i]);
+        images.setAttribute("width", "50%" );
+        images.setAttribute("alt", "Shoes Best" );
+        //----------------------------------------
+        var divDesc = document.createElement('DIV');
+        divDesc.setAttribute("class","description");
         
-          
+        var textDesc = document.createTextNode("Description");
+        //---------------------------------------
+        var pDescription = document.createElement('P');
+        //---------------------------
 		
-//            divForboutton.appendChild(bouton);
-//			divForboutton.appendChild(qte);
-//		    divForboutton.appendChild(textQte);
-		
-        //     divForboutton.appendChild(bouton2);
-        
-        
-        divForTilte.appendChild(textTitle);
-        
-		divForImages.appendChild(images);
-		
-		divForStars.appendChild(textStaring);
-		
-		divForPrices.appendChild(textPrice);
-        
+		var textDescription = document.createTextNode(description[i]);
+        //-----------------------------------
+		var spanButton = document.createElement('SPAN');
+        spanButton.setAttribute("class","btnresize btnresize1");
         //--------------------------------------
-        divForPrices.appendChild(divForboutton);
+        var buttonPlus = document.createElement('BUTTON');
+        buttonPlus.setAttribute("class","btn btn-warning btn-lg btn-block");
+		//----------------------------------------
+        var iButtPlus = document.createElement('I');
+        iButtPlus.setAttribute("class","fa fa-search-plus");
+        iButtPlus.setAttribute("aria-hidden","true");
+        //-----------------------------------------
+         var textPlus = document.createTextNode("image");
+        //------------------------------------------
+         var divSamllGally = document.createElement('DIV');
+        divSamllGally.setAttribute("class","container smallgallery");
+        var spanEmpty = document.createElement('SPAN');
+        spanEmpty.setAttribute("class","");
+        //-----------------------------------------
+        var rowSmallGally = document.createElement('DIV');
+        rowSmallGally.setAttribute("class","row");
+        rowSmallGally.setAttribute("id","main");
+        //-------------------------------------------
+        var clasSmallGally = document.createElement('DIV');
+        clasSmallGally.setAttribute("class","col-sm-12");
+        //-------------------------------------------
+        var aThumbs = document.createElement('A');
+        aThumbs.setAttribute("href","#");
+        aThumbs.setAttribute("class","thumbnail2");
+        //--------------------------------------------
+        var imageThumbs = []
         
-         var textH3 = document.createElement("H3");
-       // $("H3").val({"COMMANDER"});
-		
-      //  divForboutton.appendChild(textH3);
-        
-        
-        
-        
-        
-        
-		
-		//-----------------------------
-		wrapperRow.appendChild(mainTitle);
-        
-      
-        
-		wrapperRow.appendChild(divForTilte);
-        wrapperRow.appendChild(divForImages);
-		wrapperRow.appendChild(divForStars);
-		wrapperRow.appendChild(divForPrices);
-        
-        
-       
-        
-           col.appendChild(wrapperRow);
+        for(var j=0; j<pictures[0].length; j++){
             
-        row.appendChild(col);
-		
-	 container.appendChild(row);
+            imageThumbs[j] = document.createElement('IMG');
+            
+        }
+        
+        for (var index = 0; index< pictures[0].length; index++){
+            imageThumbs[index].setAttribute("src", "images/" + pictures[0][index]);
+        imageThumbs[index].setAttribute("width","25%");
+        imageThumbs[index].setAttribute("height","100%");
+        imageThumbs[index].setAttribute("alt","Shoes Best");
+        }
+        // console.log(imageThumbs)
+        //----------------------------------------------
+        var col2 = document.createElement('DIV');
+        col2.setAttribute("class","col-sm-6 section2");
+        
+        //---------------------------------------------
+        var h3Seller = document.createElement('H3');
+       //  $('H3').text(title[i]);
+        
+        h3Seller.innerHTML = "Contact Seller";
+        //---------------------------------------------
+        var pContact = document.createElement('P');
+        pContact.innerHTML = "Contact the seller for moer information"
+        //---------------------------------------------
+        var form = document.createElement('FORM');
+        form.setAttribute("action","php/contact_seller_page.php")
+        form.setAttribute("id","formSeller");
+        //---------------------------------------------
+        var textName = document.createTextNode("Your name: ")
+        //----------------------------------------------
+        var inputName = document.createElement('INPUT');
+         inputName.setAttribute("class","input1");
+        inputName.setAttribute("type","text");
+        inputName.setAttribute("name","Name");
+        inputName.setAttribute("placeholder","enter name");
+        
+        
+        //---------------------------------------------
+        var textEmail = document.createTextNode("Your email:  ")
+        //----------------------------------------------
+        var inputEmail = document.createElement('INPUT');
+         inputEmail.setAttribute("class","input2");
+        inputEmail.setAttribute("type","text");
+        inputEmail.setAttribute("name","email");
+        inputEmail.setAttribute("placeholder","enter email");
+        
+        //----------------------------------------------
+        var textArea = document.createElement('TEXTAREA');
+        textArea.setAttribute("NAME","message");
+        textArea.setAttribute("ROWS","6");
+        textArea.setAttribute("COLS","35");
+        textArea.setAttribute("style","resize: none;");
+        //----------------------------------------------
+        var textMessage = document.createTextNode("Hi, I would like more information on your offer please.");
+        //---------------------------------------------
+        var spanSubmit = document.createElement('SPAN');
+        spanSubmit.setAttribute("class","btnresize btnresize2");
+        //---------------------------------------------
+        var buttSubmit  = document.createElement('BUTTON');
+        buttSubmit.setAttribute("type","button");
+        buttSubmit.setAttribute("class","btn btn-warning btn-lg btn-block");
+         buttSubmit.setAttribute("value","Email2");
+        //--------------------------------------------
+        var iSubmit = document.createElement('I');
+        iSubmit.setAttribute("class","fa fa-envelope-o");
+        iSubmit.setAttribute("aria-hidden","true");
+        //-------------------------------------------
+        var textSubEmail = document.createTextNode("Email");
+        //--------------------------------
+        var disclaim = document.createElement('DIV');
+        disclaim.setAttribute("class","disclaimer");
+        //------------------------------------------
+        var pFooter = document.createElement('P');
+        pFooter.setAttribute("id","footerForm");
+        //------------------------------------------
+        var strongFoot = document.createElement('STRONG');
+        strongFoot.innerHTML = "Disclaimer: <br>";
+        var textDisclaim = document.createTextNode("Lorem ipsum mmkllkl opie kkallk Lorem ipsum mmkllkl Lorem Lorem ipsum mmkllkl opie kkallk Lorem ipsum mmkllkl Lorem  Lorem ipsum mmkllkl opie kkallk Lorem ")
+        
+        
+        
+        
+        
+        
+        
+        
 
-    body.appendChild(container);
+        col.appendChild(mainTitle);
+        //-----------------------------------------------
+        col.appendChild(divSellerPro);
+            col.appendChild(spanMap);
+                spanMap.appendChild(iMap);
+                    iMap.appendChild(textMap);
+            //***********************
+            col.appendChild(spanSell);
+                    spanSell.appendChild(isell);
+                        isell.appendChild(textSell);
+            //***********************
+                col.appendChild(spanSave);
+                        spanSave.appendChild(iFavo);
+                            iFavo.appendChild(textSave);
+        //-------------------------------------------------
+        col.appendChild(aImgFocus);
+                aImgFocus.appendChild(images);
+        
+        //-------------------------------------------------
+        col.appendChild(divDesc);
+                divDesc.appendChild(textDesc);
+        //-------------------------------------------------
+        col.appendChild(pDescription);
+               pDescription.appendChild(textDescription);
+        
+        col.appendChild(spanButton);    
+                spanButton.appendChild(buttonPlus);
+                buttonPlus.appendChild(iButtPlus);
+                buttonPlus.appendChild(textPlus);
+        //-----------------------------------------------
+        col.appendChild(divSamllGally);
+                divSamllGally.appendChild(spanEmpty);
+                    spanEmpty.appendChild(rowSmallGally);
+                rowSmallGally.appendChild(clasSmallGally)
+                clasSmallGally.appendChild(aThumbs);
+        
+        for (var i = 0; i<imageThumbs.length; i++)
+            {
+              aThumbs.appendChild(imageThumbs[i]);  
+            }
+        //------------------------------------------------
+        row.appendChild(col);   //=======col[0] col-sm-6
+		
+        //-----------------------------------------------
+        col2.appendChild(h3Seller);
+        col2.appendChild(pContact);
+        col2.appendChild(form);
+        //--------------------
+        form.appendChild(textName);
+        form.appendChild(inputName);
+        $('.input1').append('<p></p><p></p>');
+        form.appendChild(textEmail);
+        form.appendChild(inputEmail);
+        $('.input2').append('<p></p><p></p>');
+        form.appendChild(textArea);
+        textArea.appendChild(textMessage);
+        form.appendChild(spanSubmit);
+        form.appendChild(buttSubmit);
+         buttSubmit.appendChild(iSubmit);
+         buttSubmit.appendChild(textSubEmail);
+        //-------------------------------------
+        disclaim.appendChild(pFooter);
+        pFooter.appendChild(strongFoot);
+        pFooter.appendChild(textDisclaim);
+        
+        
+        col2.appendChild(disclaim);
+        row.appendChild(col2)  //=======col[1] col-sm-6 section2
+        
+        //-------------------------------------
+	 container.appendChild(row);  //====== row[0] main
+
+    body.appendChild(container); //=======section[0] mainSection
 		
 	
 	}
 	
+    
 }
 
